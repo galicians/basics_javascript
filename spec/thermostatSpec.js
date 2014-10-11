f0be86bc92de74e7bb621a1633cddfe367152041
galicians <galicians@gmail.com>
2014-10-11 16:28:28 +0100

    javascript basics

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,31 @@
+
+describe("Hello world", function() {
+    it("says hello", function() {
+        expect(helloWorld()).toEqual("Hello world!");
+    });
+});
+
+
+
+describe("thermostat", function() {
+	var thermostat;
+
+	
+	it("is set to 20 degrees", function() {
+		expect(Thermostat.degrees).toEqual(20);
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ src/thermostat.js
@@ -0,0 +1,10 @@
+
+function helloWorld() {
+    return "Hello world!";
+}
+
+
+
+function Thermostat()
+	this.degrees = 20;
+end
